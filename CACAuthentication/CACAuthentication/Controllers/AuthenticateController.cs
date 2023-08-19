f8e8f96342979b6918d18baa48f72ae01d005824
Bryan Dellinger <bryan.dellinger.civ@armwarcollege.edu>
2023-08-19 06:57:27 -0400

    Initial Commit

---
+++ CACAuthentication/CACAuthentication/Controllers/AuthenticateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace CACAuthentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticateController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(' ')[1];
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await httpClient.GetAsync("https://graph.microsoft.com/v1.0/me");
+                var content = await response.Content.ReadAsStringAsync();
+
+                return Ok(content);
+            }
+        }
+    }
+}
+
